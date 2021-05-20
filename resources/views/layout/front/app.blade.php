88125dc7d6ac890f5c75a8ed4bcbbceac7d14ce8
rajadarwin <73985648+rajadarwin@users.noreply.github.com>
2021-05-20 15:28:45 +0700

    Nambah about us dan gambar xbox

--- resources/views/layout/front/app.blade.php
+++ resources/views/layout/front/app.blade.php
@@ -29,15 +29,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
-                        <h2 class="text-white mb-4">Built with Bootstrap 4</h2>
+                        <h2 class="text-white mb-4">Our Aim is to Create Happiness</h2>
                         <p class="text-white-50">
-                            Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                            <a href="https://startbootstrap.com/theme/grayscale/">the preview page</a>
-                            . The theme is open source, and you can use it for any purpose, personal or commercial.
+                        A platform dedicated to gamers of all kind. Our aim has always been to help a gamer from his/her level as an enthusiast to his/her level as a pro gamer. Start your experience by joining our community.
                         </p>
                     </div>
                 </div>
-                <img class="img-fluid" src="{{asset('front/assets/img/ipad.png')}}" alt="..." />
+                <img class="img-fluid" src="{{asset('front/assets/img/ipad3.png')}}" alt="..." />
             </div>
         </section>
         <!-- Projects-->

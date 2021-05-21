b9bc6ca022c7783f959f4dfe603370580ecc3b0e
rajadarwin <73985648+rajadarwin@users.noreply.github.com>
2021-05-21 10:43:27 +0700

    Nambah isi konten dan logo di footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,22 +5,25 @@
             <div class="container">
                 <!-- Featured Project Row-->
                 <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="{{asset('front/assets/img/bg-masthead.jpg')}}" alt="..." /></div>
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="{{asset('front/assets/img/mabars.png')}}" alt="..." /></div>
                     <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
-                            <h4>Shoreline</h4>
-                            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                            <h4>Fun and Affordable</h4>
+                            <p class="text-black-50 mb-0">CrustyCRUD Rent will give you the best experience of playing game console with your friend or your family</p>
                         </div>
                     </div>
                 </div>
                 <!-- Project One Row-->
+                <div style="margin-bottom:40px; margin-left:auto; margin-right:auto;">
+                    <h1> Our Consoles</h1>
+                </div>
                 <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
                     <div class="col-lg-6"><img class="img-fluid" src="{{asset('front/assets/img/demo-image-01.jpg')}}" alt="..." /></div>
                     <div class="col-lg-6">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-left">
-                                    <h4 class="text-white">Misty</h4>
+                                    <h4 class="text-white">Playstation</h4>
                                     <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
                                     <hr class="d-none d-lg-block mb-0 ml-0" />
                                 </div>
@@ -35,9 +38,24 @@
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-right">
-                                    <h4 class="text-white">Mountains</h4>
+                                    <h4 class="text-white">XBOX</h4>
                                     <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
                                     <hr class="d-none d-lg-block mb-0 mr-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                                <!-- Project One Row-->
+                                <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+                    <div class="col-lg-6"><img class="img-fluid" src="{{asset('front/assets/img/demo-image-01.jpg')}}" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Nintendo</h4>
+                                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                                    <hr class="d-none d-lg-block mb-0 ml-0" />
                                 </div>
                             </div>
                         </div>
@@ -62,6 +80,9 @@
         </section> -->
         <!-- Contact-->
         <section class="contact-section bg-black">
+            <div>
+            <img style="width:200px; height:75px; margin-bottom: 75px;" src="{{asset('front/assets/img/LogoBaruPutih.png')}}" class="rounded mx-auto d-block" alt="..."> />
+            </div>
             <div class="container">
                 <div class="row">
                     <div class="col-md-4 mb-3 mb-md-0">
@@ -70,7 +91,7 @@
                                 <i class="fas fa-map-marked-alt text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Address</h4>
                                 <hr class="my-4" />
-                                <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+                                <div class="small text-black-50">Jl. Boulevard Bar. Raya No.5, RW.5, West Kelapa Gading, Kelapa Gading, North Jakarta City, Jakarta 14240, Indonesia</div>
                             </div>
                         </div>
                     </div>
@@ -80,7 +101,7 @@
                                 <i class="fas fa-envelope text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Email</h4>
                                 <hr class="my-4" />
-                                <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+                                <div class="small text-black-50"><a href="#!">rent@crusty-crud.com</a></div>
                             </div>
                         </div>
                     </div>
@@ -90,7 +111,7 @@
                                 <i class="fas fa-mobile-alt text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Phone</h4>
                                 <hr class="my-4" />
-                                <div class="small text-black-50">+1 (555) 902-8832</div>
+                                <div class="small text-black-50">+6282132343821</div>
                             </div>
                         </div>
                     </div>

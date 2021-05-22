3196dbce8e395d6f11e1f242b7f3cc4dd0bc6f14
10thjeh <glock.evans8@gmail.com>
2021-05-22 15:37:04 +0700

    fixed admin index

--- resources/views/adminindex.blade.php
+++ resources/views/adminindex.blade.php
@@ -10,13 +10,13 @@
     <div class="col">
       <h3>Want to add something?</h3>
       <h4>New game just released?</h4>
-      <h5><a href="#">Add new game...</a></h5>
+      <h5><a href="{{url('admin/addnewgame')}}">Add new game...</a></h5>
       <hr>
       <h4>New console just arrived?</h4>
-      <h5><a href="#">Add new console...</a></h5>
+      <h5><a href="{{url('admin/addnewconsole')}}">Add new console...</a></h5>
       <hr>
       <h4>New weird game created new genre?</h4>
-      <h5><a href="#">Add new genre</a></h5>
+      <h5><a href="{{url('admin/addnewgenre')}}">Add new genre</a></h5>
       <hr>
     </div>
     <div class="col">

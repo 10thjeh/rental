c24af26551ed2d54958f8b0300c0ce541a619b19
rajadarwin <rajadarwin@yahoo.com>
2021-05-22 16:48:55 +0700

    Nambah gambar carousal all category

--- resources/views/console.blade.php
+++ resources/views/console.blade.php
@@ -22,9 +22,9 @@
                         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                     </ol>
                     <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active"><img class="d-block img-fluid" src="https://via.placeholder.com/900x350" alt="First slide" /></div>
-                        <div class="carousel-item"><img class="d-block img-fluid" src="https://via.placeholder.com/900x350" alt="Second slide" /></div>
-                        <div class="carousel-item"><img class="d-block img-fluid" src="https://via.placeholder.com/900x350" alt="Third slide" /></div>
+                        <div class="carousel-item active"><img class="d-block img-fluid" src="{{asset('front/assets/img/psc4.png')}}" alt="First slide" /></div>
+                        <div class="carousel-item"><img class="d-block img-fluid" src="{{asset('front/assets/img/xboxc.png')}}" alt="Second slide" /></div>
+                        <div class="carousel-item"><img class="d-block img-fluid" src="{{asset('front/assets/img/ninc.png')}}" alt="Third slide" /></div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
